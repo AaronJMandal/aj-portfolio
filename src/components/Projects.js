b5097f9dcb5d6586d868253c8cd6d3df8d8211a7
AJM <ajmandaldis@gmail.com>
2025-01-03 21:53:15 -0500

    new reigen update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import projectFiles from "../data/projectdata";
 import { useState } from "react";
+import { CiLink } from "react-icons/ci";
+import { FaGithubSquare } from "react-icons/fa";
 
 const Projects = () => {
   const [gameplay, setGameplay] = useState(null);
@@ -19,14 +21,47 @@ const Projects = () => {
         <StyledH1>Projects</StyledH1>
         <ProjectDiv>
           {projectFiles.map((p, index) => {
+            const isPlaying = gameplay === index;
+            const isVideo = p.video && isPlaying;
             return (
               <ProjectItem key={index}>
-                <StyledImg
-                  src={gameplay === index ? p.gif : p.screenshot}
-                  onMouseEnter={() => handleMouseEnter(index)}
-                  onMouseLeave={handleMouseLeave}
-                ></StyledImg>
-                <h2>{p.title}</h2>
+                <HoverWrapper isPlaying={isPlaying}>
+                  {isVideo ? (
+                    <StyledVideo
+                      src={p.video}
+                      onMouseEnter={() => handleMouseEnter(index)}
+                      onMouseLeave={handleMouseLeave}
+                      autoPlay
+                      muted
+                      loop
+                    ></StyledVideo>
+                  ) : (
+                    <StyledImg
+                      src={isPlaying ? p.gif || p.screenshot : p.screenshot}
+                      onMouseEnter={() => handleMouseEnter(index)}
+                      onMouseLeave={handleMouseLeave}
+                    ></StyledImg>
+                  )}
+                </HoverWrapper>
+                <h2>
+                  <a
+                    style={{ textDecoration: "none", color: "white" }}
+                    href={p.link}
+                  >
+                    {p.title}
+                    {!p.link === false ? <CiLink /> : null}
+                  </a>
+                </h2>
+                {!p.link === false ? (
+                  <h2>
+                    <a
+                      style={{ color: "white", fontSize: "50px" }}
+                      href={p.github}
+                    >
+                      <FaGithubSquare />
+                    </a>
+                  </h2>
+                ) : null}
               </ProjectItem>
             );
           })}
@@ -66,6 +101,39 @@ const StyledImg = styled.img`
   }
 `;
 
+const StyledVideo = styled.video`
+  height: 500px;
+  transition: transform 0.5s ease-in-out, opacity 0.5s ease-in-out;
+  border-radius: 5px;
+  padding: 10px;
+  user-drag: none;
+  -webkit-user-drag: none;
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  transform: scale(1);
+
+  &:hover {
+    transform: scale(1.3);
+  }
+
+  @media (max-width: 480px) {
+    height: auto;
+    width: 80%;
+  }
+`;
+
+const HoverWrapper = styled.div`
+  position: relative;
+  opacity: ${(props) => (props.isPlaying ? 1 : 0.8)};
+  transition: transform 0.5s ease-in-out, opacity 0.5 ease-in-out;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
 const StyledH1 = styled.h1`
   font-size: 50px;
   flex: inline-block;
@@ -78,6 +146,7 @@ const ProjectDiv = styled.div`
   display: flex;
   flex-direction: row;
   padding: 20px;
+  transition: all 0.3s ease-in-out;
 `;
 
 const ProjectItem = styled.div`
--- src/data/projectdata.js
+++ src/data/projectdata.js
@@ -1,6 +1,8 @@
 import projectnyancat1 from "../imgs/projectnyancat1.png";
 import nyancatgameplay from "../imgs/nyancatgameplay.gif";
 import nextproj from "../imgs/nextproj.png";
+import sascgif from "../imgs/sascgif.mp4";
+import sascpic from "../imgs/sascpic.png";
 
 const projectFiles = [
   {
@@ -8,6 +10,15 @@ const projectFiles = [
     screenshot: projectnyancat1,
     gif: nyancatgameplay,
   },
+  {
+    title: "Spirits and Such Consultation Agency",
+    screenshot: sascpic,
+    gif: null,
+    video: sascgif,
+    link: "https://project-capstone-ten.vercel.app/",
+    github: "https://github.com/AaronJMandal/project-capstone",
+  },
+  ,
   {
     title: "More to come!",
     screenshot: nextproj,
